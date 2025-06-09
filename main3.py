57e9a53c4858cb4b98d6026b10df468446c01bcf
Aaditya <aadityajnec0008@gmail.com>
2025-06-09 15:36:18 +0530

    Section E Names Changed

--- main3.py
+++ main3.py
@@ -20,63 +20,63 @@ from SWOTkpi.Source import (
 
 app = FastAPI(title="SWOT Specific Indicators APIs")
 
-@app.get("/kpi/review-theme-analysis")
-def analyze_review_themes() -> Dict[str, Any]:
-    return analyze_review_themes_logic()
-
-@app.get("/Lower_Engagement_Rate/")
-def analyze_social_media_metrics() -> Dict[str, Any]:
-    return analyze_social_media_metrics_logic()
-
-@app.get("/website_usability_issues/")
-def analyze_website_issues() -> Dict[str, Any]:
-    return analyze_website_issues_logic()
-
-@app.get("/analyze_response_rates/")
-async def analyze_response_rates() -> Dict[str, Any]:
-    return await analyze_response_rates_logic()
-
-@app.get("/news_sentiment_spikes/")
-async def news_sentiment_spikes() -> Dict[str, Any]:
-    return await news_sentiment_spikes_logic()
-
-@app.get("/search_volume_rank/")
+@app.get("/E1_Strength/High_Brand_Search_Volume_Rank/")
 async def search_volume_rank() -> Dict[str, Any]:
     return await search_volume_rank_logic()
 
-@app.get("/analyze_sentiment_score/")
+@app.get("/E2_Strength/Positive_Overall_Sentiment_Score/")
 async def analyze_sentiment_score() -> Dict[str, Any]:
     return await analyze_sentiment_score_logic()
 
-@app.get("/product_range_visibility/")
+@app.get("/E3_Strength/Wide_Product_Range_Visibility/")
 async def product_range_visibility() -> Dict[str, Any]:
     return await product_range_visibility_logic()
 
-@app.get("/analyze_ecommerce_presence/")
+@app.get("/E4_Strength/Strong_Ecommerce_Presence/Ratings/")
 async def analyze_ecommerce_presence() -> Dict[str, Any]:
     return await analyze_ecommerce_presence_logic()
 
-@app.get("/high_review_volume/")
+@app.get("/E5_Strength/High_review_volume/")
 async def high_review_volume() -> Dict[str, Any]:
     return await high_review_volume_logic()
 
-@app.get("/analyze_high_growth_adjacent_categories/")
+@app.get("/E6_Weakness/Key_Negative_Review_Themes_Frequency")
+def analyze_review_themes() -> Dict[str, Any]:
+    return analyze_review_themes_logic()
+
+@app.get("/E7_Weakness/Lower_Engagement_Rate_vs_Select_Competitors/")
+def analyze_social_media_metrics() -> Dict[str, Any]:
+    return analyze_social_media_metrics_logic()
+
+@app.get("/E8_Weakness/Identified_Website_usability_issues/")
+def analyze_website_issues() -> Dict[str, Any]:
+    return analyze_website_issues_logic()
+
+@app.get("/E9_Weakness/Gaps_in_Online_Customer_Service_Response/")
+async def analyze_response_rates() -> Dict[str, Any]:
+    return await analyze_response_rates_logic()
+
+@app.get("/E10_Weakness/Negative_News_Sentiment_Spikes/")
+async def news_sentiment_spikes() -> Dict[str, Any]:
+    return await news_sentiment_spikes_logic()
+
+@app.get("/E11_Opportunities/High_Growth_Rate_In_Adjacent_Categories/")
 async def analyze_high_growth_adjacent_categories() -> Dict[str, Any]:
     return await analyze_high_growth_adjacent_categories_logic()
 
-@app.get("/analyze_trend_alignment/")
+@app.get("/E12_Opportunities/Rising_Trend_Alignment/")
 async def analyze_trend_alignment() -> Dict[str, Any]:
     return await analyze_trend_alignment_logic()
 
-@app.get("/analyze_unmet_needs/")
+@app.get("/E13_Opportunities/Unmet_Needs_Identified_In_Reviews/")
 async def analyze_unmet_needs() -> Dict[str, Any]:
     return await analyze_unmet_needs_logic()
 
-@app.get("/analyze_competitor_weaknesses/")
+@app.get("/E14_Opportunities/Competitor_Weaknesses_Identified/")
 async def analyze_competitor_weaknesses() -> Dict[str, Any]:
     return await analyze_competitor_weaknesses_logic()
 
-@app.get("/geographic_search_opportunity/")
+@app.get("/E15_Opportunities/Untapped_Geographic_Search_Interest/")
 def geographic_search_opportunity() -> Dict[str, Any]:
     return  geographic_search_opportunity_logic()
 
